91429b31f194db88164c901ab9a1c2fad9e364a7
jptucker <john.pelton.tucker@gmail.com>
2016-05-07 12:41:48 -0500

    Card selections into draft, started on charts

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -23,24 +23,41 @@ class CardsController < ApplicationController
     card = Card.new
     card.image_url = magicCard.image_url
     card.multiverse_id = magicCard.multiverse_id
-    card.is_planeswalker = magicCard.types.include('Planeswalker')
-    card.is_land = magicCard.types.include('Land')
-    card.is_enchantment = magicCard.types.include('Enchantment')
-    card.is_artifact = magicCard.types.include('Artifact')
-    card.is_sorcery = magicCard.types.include('Sorcery')
-    card.is_instant = magicCard.types.include('Instant')
-    card.is_creature = magicCard.types.include('Creature')
-    card.is_blue = magicCard.colors.include('blue')
-    card.is_white = magicCard.colors.include('white')
-    card.is_red = magicCard.colors.include('red')
-    card.is_black = magicCard.colors.include('black')
-    card.is_green = magicCard.colors.include('green')
-    card.is_colorless = !card.is_blue && !card.is_white && !card.is_red && !card.is_black && ! card.isgreen
+
+    if !magicCard.types.nil?
+      card.is_planeswalker = magicCard.types.include? 'Planeswalker'
+      card.is_land = magicCard.types.include? 'Land'
+      card.is_enchantment = magicCard.types.include? 'Enchantment'
+      card.is_artifact = magicCard.types.include? 'Artifact'
+      card.is_sorcery = magicCard.types.include? 'Sorcery'
+      card.is_instant = magicCard.types.include? 'Instant'
+      card.is_creature = magicCard.types.include? 'Creature'
+    else
+      card.is_planeswalker = card.is_land = card.is_enchantment = card.is_artifact = card.is_sorcery = card.is_instant = card.is_creature = false
+    end
+
+    if !magicCard.colors.nil?
+      card.is_blue = magicCard.colors.include? 'blue'
+      card.is_white = magicCard.colors.include? 'white'
+      card.is_red = magicCard.colors.include? 'red'
+      card.is_black = magicCard.colors.include? 'black'
+      card.is_green = magicCard.colors.include? 'green'
+    else
+      card.is_blue = card.is_white = card.is_red = card.is_black = card.is_green = false
+      card.is_colorless = true
+    end
+
     card.cmc = magicCard.cmc
     card.name = magicCard.name
 
     if card.save
-      redirect_to cards_url, notice: "Card created successfully."
+      selection = Selection.new
+      selection.is_sideboard = false
+      selection.draft_id = "3"
+      selection.card_id = card.id
+      if selection.save
+        redirect_to cards_url, notice: "Card created successfully."
+      end
     else
       render 'index'
     end
--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -6,6 +6,97 @@ class DraftsController < ApplicationController
 
   def show
     @draft = Draft.find_by(id: params[:id])
+    selections = Selection.where(:draft_id => @draft.id)
+    @cards = Array.new
+    selections.each {|sel| @cards.push(Card.find_by(:id => sel.card_id)) }
+
+    @chart_power_seriesdata = [0,0,0,0,0,0,0,0]
+
+    num_blue = 0
+    num_black = 0
+    num_red = 0
+    num_green = 0
+    num_white = 0
+    num_colorless = 0
+    num_multi = 0
+    count_colors = 0
+
+    num_creatures = 0
+    num_instants = 0
+    num_sorceries = 0
+    num_artifacts = 0
+    num_enchantments = 0
+    num_lands = 0
+    num_other = 0
+
+    @cards.each { |card|
+      cmc = [card.cmc.to_i,7].min
+      @chart_power_seriesdata[cmc] += 1
+      count_colors +=1 if card.is_blue
+      count_colors +=1 if card.is_black
+      count_colors +=1 if card.is_red
+      count_colors +=1 if card.is_green
+      count_colors +=1 if card.is_white
+
+
+      if card.is_colorless
+        num_colorless +=1
+      else
+        if count_colors > 1
+          num_multi += 1
+        else
+          if card.is_blue
+            num_blue += 1
+          else
+            if card.is_black
+              num_black +=1
+            else
+              if card.is_red
+                num_red +=1
+              else
+                if card.is_green
+                  num_green += 1
+                else
+                  if card.is_white
+                    num_white +=1
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+
+      if card.is_creature
+        num_creatures += 1
+      end
+      if card.is_instant
+        num_instants += 1
+      end
+      if card.is_sorcery
+        num_sorceries += 1
+      end
+      if card.is_artifact
+        num_artifacts += 1
+      end
+      if card.is_enchantment
+        num_enchantments += 1
+      end
+      if card.is_land
+        num_lands += 1
+      end
+      if !card.is_creature && !card.is_instant && !card.is_sorcery && !card.is_artifact && !card.is_enchantment && !card.is_land
+        num_other += 1
+      end
+    }
+
+    @chart_power_xaxis_categories = ["0","1","2","3","4","5","6","7+"]
+
+    @chart_color_data = [['Blue',num_blue], ['Black',num_black],['Red',num_red],['Green',num_green],['White',num_white],['Colorless',num_colorless],['Multi',num_multi]]
+
+    @chart_type_xaxis_categories = ["Creatures","Instants","Sorceries","Artifacts","Enchantments","Lands","Other"]
+    @chart_type_seriesdata = [num_creatures,num_instants,num_sorceries,num_artifacts,num_enchantments,num_lands,num_other]
+
   end
 
   def new
